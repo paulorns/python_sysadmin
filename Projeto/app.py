6ca811061210ac7d12f779c90c3b0280ae2a45b5
Paulo Nunes <htx.prs@gmail.com>
2019-12-06 19:38:16 -0200

    Projeto e Aula05

--- Projeto/app.py
+++ Projeto/app.py
@@ -1,11 +1,16 @@
 import flask
 # import docker
 
+# Importando as rotas das blueprints
 from blueprints.docker_blueprint import docker_routes
+from blueprints.jenkins_blueprint import jenkins_routes
 
+# Controle de aplicacao
 app = flask.Flask(__name__)
 
+# Registro de rotas
 app.register_blueprint(docker_routes)
+app.register_blueprint(jenkins_routes)
 
 @app.route('/')
 def index():
---
+++ Projeto/blueprints/jenkins_blueprint.py
@@ -0,0 +1,59 @@
+import flask
+import jenkins
+
+jenkins_routes = flask.Blueprint(name='jenkins', import_name=__name__,url_prefix='/jenkins')
+
+@jenkins_routes.route('/')
+def index():
+    try:
+        # Criando a conexao com o Jenkins
+        client = jenkins.Jenkins('http://localhost:8080',username='admin', password='admin')
+        # Coletando os jobs que temos no jenkins
+        jobs_list = client.get_jobs()
+        jobs = []
+
+        for job in jobs_list:
+            jobs.append(client.get_job_info(job['fullname']))
+    except Exception as e:
+        print(e)
+    
+    return flask.render_template('jenkins.jinja', jobs=jobs)
+
+@jenkins_routes.route('/build/<string:job_name>')
+def jenkins_build(job_name):
+    try:
+        # Criando a conexao com o Jenkins
+        client = jenkins.Jenkins('http://localhost:8080',username='admin', password='admin')
+        client.build_job(job_name)
+    except Exception as e:
+        print(e)
+    
+    return flask.redirect(flask.url_for('jenkins.index'))
+
+@jenkins_routes.route('/update/<string:job_name>')
+def jenkins_update(job_name):
+    try:
+        # Criando a conexao com o Jenkins
+        client = jenkins.Jenkins('http://localhost:8080',username='admin', password='admin')
+        job = {
+            'name':job_name,
+            'xml':client.get_job_config(job_name)
+        }
+    except Exception as e:
+        print(e)
+    # Apos o teste de conexcao, se der certo, ele redireciona para o
+    # formulario de atualizacao e passa a variavel 'job'
+    return flask.render_template('jenkins_update.jinja', job)
+
+@jenkins_routes.route('/rebuild', method=['POST'])
+def jenkins_rebuild():
+    data = flask.request.form
+    try:
+        # Criando a conexao com o Jenkins
+        client = jenkins.Jenkins('http://localhost:8080',username='admin', password='admin')
+        client.reconfig_job(data['name'], data['xml'])
+        # Se der certo, ele redireciona para o Index
+        return flask.redirect(flask.url_for('jenkins.index'))
+    except Exception as e:
+        # Caso de erro, ele retorna para o formulario
+        return flask.redirect(flask.url_for('jenkins.update', job_name=data['name']))
